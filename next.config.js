e1ce1961e5d7bc46286effce57b14fe249001ac1
Comet1212 <bwolf19951212@gmail.com>
2022-12-07 08:42:30 +0900

    SEO

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true, // triggers 308
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
